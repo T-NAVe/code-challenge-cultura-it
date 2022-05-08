b33b3e33840cdc2b7825a50a5ef72ce815e7ab0f
T-NAVe <65693205+T-NAVe@users.noreply.github.com>
2022-05-07 22:07:17 -0300

    Added essential URLs

---
+++ src/services/settings.js
@@ -0,0 +1,8 @@
+/**
+- GET/posts -- https://jsonplaceholder.typicode.com/posts -- obtener todos los posts
+- GET/posts/1 -- https://jsonplaceholder.typicode.com/posts/1 -- obtener un post filtrando por id
+- GET/posts/1/comments -- https://jsonplaceholder.typicode.com/posts/1/comments  -- obtener los comentarios de un post filtrado por id
+ */
+
+export const API_URL = 'https://jsonplaceholder.typicode.com'
+export const AVATAR_RUL = 'https://avatars.dicebear.com/api/initials/'
