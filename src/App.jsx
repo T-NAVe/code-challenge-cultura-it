f56e5340de7bf92cdd542d5a49ba8605d68328dc
T-NAVe <65693205+T-NAVe@users.noreply.github.com>
2022-05-09 18:33:00 -0300

    added navbar with styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,42 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, NavLink } from 'react-router-dom'
 import './App.css'
+import Albums from './pages/Albums/Albums'
+import Posts from './pages/Posts/Posts'
 import User from './pages/User/User'
 
 function App () {
   return (
     <div className='App'>
+      <header>
+        <h1>App 🐱‍👤</h1>
+        <nav>
+          <ul>
+            <li>
+              <NavLink
+                // I may want to use active default class for something else, so i'm replacing it with custon nav-active class
+                // this only works this way with NavLink (react router v6)
+                className={({ isActive }) => {
+                  return isActive ? 'nav-active' : undefined
+                }} to='/'
+              >Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                className={({ isActive }) => {
+                  return isActive ? 'nav-active' : undefined
+                }}
+                to='/albums'
+              >Albums
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
       <Routes>
-        {/* <Route exact path='/' element={<Home />} /> */}
-        <Route exact path='/User/:id/' element={<User />} />
+        <Route exact path='/user/:id/' element={<User />} />
+        <Route exact path='/albums' element={<Albums />} />
+        <Route exact path='/' element={<Posts />} />
         <Route path='*' element={<h1>Not Found</h1>} />
       </Routes>
     </div>
